2abebf9242c1c4f5fab3c2a8025cd4c7b6ea7193
keiu <keiu.kunnarpuu@voco.ee>
2024-03-25 11:20:42 +0200

    Setup game window

--- game.py
+++ game.py
@@ -1,20 +1,25 @@
 # import and initialize the pygame library
 import pygame
-pygame.init()
+from settings import Settings
 
-# set up drawing window
-screen = pygame.display.set_mode([800, 600])
-
-# run util user asks to quit
-running = True
-while running:
-    
-    # did the user click the window close button?
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    # flip the display       
-    pygame.display.flip()
-
-# done! time to quit
-pygame.quit()
+def run_game():
+    pygame.init()
+    gm_settings = Settings()
+    # set up drawing window
+    screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
+    pygame.display.set_caption(gm_settings.caption)
+    # run util user asks to quit
+    running = True
+    while running:
+        screen.fill(gm_settings.bg_color)
+        # did the user click the window close button?
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            
+            # flip the display
+            pygame.display.flip()
+        # done! time to quit
+        pygame.quit()
+        
+run_game()
