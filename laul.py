b0a67d7842ff53d976f06af430bf8344efa34012
keiukunnarpuu <148068827+keiukunnarpuu@users.noreply.github.com>
2024-04-23 07:16:51 +0300

    Delete laul.py

--- laul.py
+++
@@ -1,8 +0,0 @@
-class Laul:
-    def __init__(self, esitaja, pealkiri, kuulamiste_arv):
-        self.esitaja = esitaja
-        self.pealkiri = pealkiri
-        self.kuulamiste_arv = kuulamiste_arv
-
-    def näita(self):
-        print(f"Esitaja: {self.esitaja}, Pealkiri: {self.pealkiri}, Kuulamiste arv: {self.kuulamiste_arv}")
