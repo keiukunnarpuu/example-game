14971031cb7ad9105ee0ab6d77a5804ea3a46f78
keiukunnarpuu <148068827+keiukunnarpuu@users.noreply.github.com>
2024-05-05 20:06:39 +0300

    settings.py

--- settings.py
+++ settings.py
@@ -1,10 +1,20 @@
 class Settings:
     """Class for game settings"""
     
+    
     def __init__(self):
         """Initialize game settings"""
         # Screen settings
         self.screen_width = 800
-        self.screen_height = 600
-        self.bg_color = (0, 0, 139)
-        self.caption = 'Bubble bluster'
+        self.screen_height = 500
+        self.bg_color = (153, 170, 181)
+        self.caption = '''Vanda's Paradise'''
+        
+        self.bubble_min_r = 10
+        self.bubble_max_r = 50
+        
+        self.bonus_score = 1000
+        
+        self.speed_min = 1
+        self.speed_max = 5
+        self.speed_level = 1
