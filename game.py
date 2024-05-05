0122c1c03834a7da9d4fbb4b5446449ca7a67c40
keiukunnarpuu <148068827+keiukunnarpuu@users.noreply.github.com>
2024-05-05 20:08:08 +0300

    game.py

--- game.py
+++ game.py
@@ -1,26 +1,48 @@
-# import and initialize the pygame library
-import pygame, sys
+import pygame
 from settings import Settings
+from button import Button
+from player import Player
+from bubble import Bubble
+from scoreboard import Scoreboard
+from game_stats import GameStats
+import game_functions as gf
+
 
 def run_game():
     pygame.init()
     gm_settings = Settings()
-    # set up drawing window
+
+
+
+    # Set up the drawing window
     screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
     pygame.display.set_caption(gm_settings.caption)
-    # run util user asks to quit
-    running = True
-    while running:
-        screen.fill(gm_settings.bg_color)
-        # did the user click the window close button?
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            
-            # flip the display
-            pygame.display.flip()
-        # done! time to quit
-        pygame.quit()
-        sys.exit()
-        
+    
+    # Set up Play button
+    play_button = Button(gm_settings, screen, "Play")
+    
+    # Set up game score
+    stats = GameStats()
+    
+    # Set up scoreboard
+    sb = Scoreboard(gm_settings, screen, stats)
+    
+    # Set up clock to decent frame rate
+    clock = pygame.time.Clock()
+
+    # Initiate player
+    player = Player(screen)
+    # Create groups to hold bubbles
+    bubbles = pygame.sprite.Group()
+    
+    #Run until user asks to quit
+    while True:
+        gf.check_events(gm_settings, screen, player, bubbles, stats, play_button)
+        if stats.game_active:
+            player.update()
+            gf.update_bubbles(player, bubbles, stats, sb, gm_settings)
+            bubbles.update()
+        else:
+            bubbles.empty()
+        gf.update_screen(gm_settings, screen, player, bubbles, clock, stats, play_button, sb)      
 run_game()
